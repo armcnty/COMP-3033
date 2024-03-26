c58cfc8e1b12c99507c3a3daa4b3bfd26076d18f
armcnty <62534101+armcnty@users.noreply.github.com>
2024-03-26 14:46:41 -0300

    Delete Angular-Example/app directory

--- Angular-Example/app/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-
-import { AppComponent } from './app/app.component';
-import { HttpClientModule } from '@angular/common/http';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
